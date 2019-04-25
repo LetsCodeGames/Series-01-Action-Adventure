cb8a7ed76f4e418e1198fd8e9d899bb13eb5c8ff
Jacob Bodkin <bodkin.j@gmail.com>
2019-04-25 15:03:57 -0500

    nevar exists when walking

--- Assets/Tutorial/Scripts/Rooms/RoomDoorBlockingObject.cs
+++ Assets/Tutorial/Scripts/Rooms/RoomDoorBlockingObject.cs
@@ -10,8 +10,10 @@ public class RoomDoorBlockingObject : MonoBehaviour
         RoomParent roomParent = GetComponentInParent<RoomParent>();
 
         float randomValue = Random.Range( 0f, 1f );
+        Debug.Log("X" + roomParent.X);
+        Debug.Log("Y" + roomParent.Y);
 
-        if( randomValue < DestroyProbability ||
+        if ( randomValue < DestroyProbability ||
             ( roomParent.X == 0 && roomParent.Y == 0 ) ||
             ( roomParent.X == 0 && roomParent.Y == 1 ) ||
             ( roomParent.X == 0 && roomParent.Y == -1 ) ||
