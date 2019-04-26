48421e039f17c59bd8af83a40e1ee2b5f31b4d82
Jacob Bodkin <bodkin.j@gmail.com>
2019-04-26 12:46:50 -0500

    added more custom visuals and audio

---
+++ Assets/Tutorial/Scripts/Objects/Iteractable/InteractableOwl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableOwl : InteractableBase
+{
+    public override void OnInteract( Character character )
+    {
+
+        this.PlayInteractionSound();
+        
+    }
+}
